1717aacc3790814f3bf51b7fc7cfa8e4a49b07d2
seunghaen <tmdgus9701@naver.com>
2023-01-11 13:54:12 +0900

    make new app

---
+++ src/slices/user.ts
@@ -0,0 +1,28 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  name: '',
+  email: '',
+  accessToken: '',
+  money: 0,
+};
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.email = action.payload.email;
+      state.name = action.payload.name;
+      state.accessToken = action.payload.accessToken;
+    },
+    setAccessToken(state, action) {
+      state.accessToken = action.payload;
+    },
+    setMoney(state, action) {
+      state.money = action.payload;
+    },
+  },
+  extraReducers: builder => {},
+});
+
+export default userSlice;
